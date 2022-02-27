40e6cb1b2611426f58089037294cee1f80afedb5
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-26 22:25:42 -0300

    ✅ finished FeatureCard component

--- src/components/feature-card.js
+++ src/components/feature-card.js
@@ -7,7 +7,15 @@ export default function FeatureCard({
   title,
   text,
 }) {
-  return <Box sx={styles.card}></Box>;
+  return (
+    <Box sx={styles.card}>
+      <Image src={src} alt={altText} sx={styles.img} />
+      <Box sx={styles.wrapper}>
+        <Heading sx={styles.wrapper.title}>{title} </Heading>
+        <Text sx={styles.wrapper.subTitle}>{text} </Text>
+      </Box>
+    </Box>
+  );
 }
 
 const styles = {
