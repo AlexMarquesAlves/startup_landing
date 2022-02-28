21ebdb28e094a6757111cf98611ba60387540996
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-28 10:54:11 -0300

    📝Start Coding Footer section

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,55 +1,53 @@
 /** @jsx jsx */
-import { jsx, Box, Container, Image, Text } from 'theme-ui';
-import { Link } from 'components/link';
-import data from './footer.data';
-import FooterLogo from 'assets/logo.svg';
+import { jsx, Box, Container, Image, Text } from "theme-ui";
+import { Link } from "components/link";
+import data from "./footer.data";
+import FooterLogo from "assets/logo.svg";
 
 export default function Footer() {
-  return (
-    <h1>Footer</h1>
-  );
+  return <footer></footer>;
 }
 
 const styles = {
   footer: {
     footerBottomArea: {
-      borderTop: '1px solid',
-      borderTopColor: 'border_color',
-      display: 'flex',
+      borderTop: "1px solid",
+      borderTopColor: "border_color",
+      display: "flex",
       pt: [7, null, 8],
-      pb: ['40px', null, '100px'],
-      textAlign: 'center',
-      flexDirection: 'column',
+      pb: ["40px", null, "100px"],
+      textAlign: "center",
+      flexDirection: "column",
     },
     menus: {
       mt: [3, 4],
       mb: 2,
       nav: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexWrap: 'wrap',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexWrap: "wrap",
       },
     },
 
     link: {
-      fontSize: [1, '15px'],
-      color: 'text',
-      fontWeight: '400',
+      fontSize: [1, "15px"],
+      color: "text",
+      fontWeight: "400",
       mb: 2,
-      cursor: 'pointer',
-      transition: 'all 0.35s',
-      display: 'block',
-      textDecoration: 'none',
+      cursor: "pointer",
+      transition: "all 0.35s",
+      display: "block",
+      textDecoration: "none",
       lineHeight: [1.5, null, 1.8],
       px: [2, null, 4],
-      ':hover': {
-        color: 'primary',
+      ":hover": {
+        color: "primary",
       },
     },
     copyright: {
-      fontSize: [1, '15px'],
-      width: '100%',
+      fontSize: [1, "15px"],
+      width: "100%",
     },
   },
 };
