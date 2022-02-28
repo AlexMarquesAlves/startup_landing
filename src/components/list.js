e61826dc4b0766f19d77a803d0055621748a03ce
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-28 09:19:28 -0300

    📝Start Coding list component

--- src/components/list.js
+++ src/components/list.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Flex, Box, IconButton } from 'theme-ui';
+import React from "react";
+import { Flex, Box, IconButton } from "theme-ui";
 
 export default function List({ items = [], parentStyle, childStyle }) {
-  return (
-    <h1>List</h1>
-  );
+  return <Box></Box>;
 }
 
 const styles = {
   listIcon: {
-    width: ['25px', '35px'],
-    height: 'auto',
-    color: 'primary',
+    width: ["25px", "35px"],
+    height: "auto",
+    color: "primary",
     padding: 0,
     fontSize: [3, 5],
-    ml: '-1px',
+    ml: "-1px",
     flexShrink: 0,
-    justifyContent: 'flex-start',
-    mt: '2px',
+    justifyContent: "flex-start",
+    mt: "2px",
   },
 };
