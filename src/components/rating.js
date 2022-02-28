15563409515aa0337d8ed93fed4d3998fffa93be
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-28 10:42:28 -0300

    📝Start Coding Rating component

--- src/components/rating.js
+++ src/components/rating.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { FaStar } from 'react-icons/fa';
+import React from "react";
+import { FaStar } from "react-icons/fa";
 
-const Rating = () => {
-  return (
-    <h1>Rating</h1>
-  );
+const Rating = ({ rating }) => {
+  const totalRating = [];
+
+  for (let i = 0; i < 5; i++) {
+    if (i < rating) {
+      totalRating.push(
+        <li className="start" key={i}>
+          <FaStar />
+        </li>
+      );
+    } else {
+      totalRating.push(
+        <li className="start-o" key={i}>
+          <FaStar />
+        </li>
+      );
+    }
+  }
+  return <h1>Rating</h1>;
 };
 
 export default Rating;
