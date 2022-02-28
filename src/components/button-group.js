a31618267fbf8497c45d4a888e729acac32859b0
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-28 10:03:05 -0300

    ✅ finished button-group component

--- src/components/button-group.js
+++ src/components/button-group.js
@@ -3,7 +3,20 @@ import { jsx, Box, Container, Flex } from "theme-ui";
 import { IoIosArrowRoundBack, IoIosArrowRoundForward } from "react-icons/io";
 
 export default function ButtonGroup({ next, previous }) {
-  return <Flex sx={{ width: "100%" }}></Flex>;
+  return (
+    <Flex sx={{ width: "100%" }}>
+      <Container>
+        <Box sx={styles.buttonGroup} className={"button__group"}>
+          <button onClick={previous} aria-label={"Previous"}>
+            <IoIosArrowRoundBack />
+          </button>
+          <button onClick={next} aria-label={"Next"}>
+            <IoIosArrowRoundForward />
+          </button>
+        </Box>
+      </Container>
+    </Flex>
+  );
 }
 
 const styles = {
