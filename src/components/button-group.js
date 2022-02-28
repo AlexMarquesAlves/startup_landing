fbe8cbf3161f69964f1a984307c4c52a19349920
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-28 09:57:14 -0300

    📝Start Coding ButtonGroup component

--- src/components/button-group.js
+++ src/components/button-group.js
@@ -1,30 +1,28 @@
 /** @jsx jsx */
-import { jsx, Box, Container, Flex } from 'theme-ui';
-import { IoIosArrowRoundBack, IoIosArrowRoundForward } from 'react-icons/io';
+import { jsx, Box, Container, Flex } from "theme-ui";
+import { IoIosArrowRoundBack, IoIosArrowRoundForward } from "react-icons/io";
 
 export default function ButtonGroup({ next, previous }) {
-  return (
-    <h1>ButtonGroup</h1>
-  );
+  return <Flex sx={{ width: "100%" }}></Flex>;
 }
 
 const styles = {
   buttonGroup: {
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
     mb: -4,
     button: {
-      bg: 'transparent',
-      border: '0px solid',
+      bg: "transparent",
+      border: "0px solid",
       fontSize: 40,
-      cursor: 'pointer',
-      px: '2px',
-      color: '#BBC7D7',
-      transition: 'all 0.25s',
-      '&:hover': {
-        color: 'text',
+      cursor: "pointer",
+      px: "2px",
+      color: "#BBC7D7",
+      transition: "all 0.25s",
+      "&:hover": {
+        color: "text",
       },
-      '&:focus': {
+      "&:focus": {
         outline: 0,
       },
     },
