34e2406e798f26c8a61afdd1b580f16719d3971e
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-28 11:07:35 -0300

    ✅ finished Footer section

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,10 +2,35 @@
 import { jsx, Box, Container, Image, Text } from "theme-ui";
 import { Link } from "components/link";
 import data from "./footer.data";
-import FooterLogo from "assets/logo.svg";
+import LogoDark from "assets/logo.svg";
 
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer sx={styles.footer}>
+      <Container>
+        <Box sx={styles.footer.footerBottomArea}>
+          <Link path="/">
+            <Image src={LogoDark} alt={"logo"} />
+          </Link>
+          <Box sx={styles.footer.menus}>
+            <nav>
+              {data.menuItem.map((item, i) => (
+                <Link
+                  path={item.path}
+                  key={i}
+                  label={item.label}
+                  sx={styles.footer.link}
+                ></Link>
+              ))}
+            </nav>
+          </Box>
+          <Text sx={styles.footer.copyright}>
+            Copyright by {new Date().getFullYear()} Company Name
+          </Text>
+        </Box>
+      </Container>
+    </footer>
+  );
 }
 
 const styles = {
